9bec388d62fa58eb50e245539f600ee6c8118f7f
Kyle Oba <koba@mudphone.com>
2014-01-30 21:50:55 -1000

    Show formatted times on calendar view.

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -1,2 +1,19 @@
 module CalendarHelper
+  def start_time_for event
+    date_time = event.start.strftime "%A, %b %-d, %Y"
+    date_time += event.start.strftime " at %l:%M %P" unless event.all_day
+    date_time
+  end
+
+  def time_til_start_of event
+    now = Time.zone.now
+    in_future = now < event.start
+    s = distance_of_time_in_words now, event.start
+    if in_future
+      s = "in " + s
+    else
+      s += " ago"
+    end
+    s
+  end
 end
